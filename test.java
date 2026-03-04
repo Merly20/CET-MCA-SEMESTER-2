5fbba3f4d18b5621654885bfa7e34cfb98f2ce8a
merly cm <merlymurali000@gmail.com>
2026-03-04 15:07:56 +0530

    javaaaaa

---
+++ test.java
@@ -0,0 +1,18 @@
+
+public class test  {
+    public static void main(String a[]){
+        System.out.print("Hello world");
+
+    }
+}
+
+
+
+class Merly{
+    void merlyfn(){
+            System.out.print("Hello merly");
+
+    }
+
+}
+
